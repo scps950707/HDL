bcb6d101e135db0fd76891c2426bca16ee1c8a49
義洋 顏 <scps950707@gmail.com>
2015-10-26 22:19:58 +0800

    update

--- HW2/pipealu.v
+++ HW2/pipealu.v
@@ -1,3 +1,4 @@
+`timescale 1ns/1ps
 module pipealu(instr,AluOut, Zero,clk,Carryout,Overflow);
 	input [15:0] instr;
 	input clk;
@@ -18,18 +19,18 @@ module pipealu(instr,AluOut, Zero,clk,Carryout,Overflow);
 		regfile[1] = 32'h98765432;
 		regfile[2] = 15;
 		regfile[3] = 20;
-		regfile[4] = $random;
-		regfile[5] = $random;
-		regfile[6] = $random;
-		regfile[7] = $random;
-		regfile[8] = $random;
-		regfile[9] = $random;
-		regfile[10] = $random;
-		regfile[11] = $random;
-		regfile[12] = $random;
-		regfile[13] = $random;
-		regfile[14] = $random;
-		regfile[15] = $random;
+		regfile[4] = 0;
+		regfile[5] = 32'h40000000;
+		regfile[6] = 32'h40000000;
+		regfile[7] = 32'h40000000;
+		regfile[8] = 32'hc0000000;
+		regfile[9] = 5;
+		regfile[10] = 6;
+		regfile[11] = 100;
+		regfile[12] = 8;
+		regfile[13] = 99;
+		regfile[14] = 5;
+		regfile[15] = 18;
 		Carryout = 0;
 		Zero = 0;
 		Overflow = 0;
@@ -46,8 +47,16 @@ module pipealu(instr,AluOut, Zero,clk,Carryout,Overflow);
 	always@(posedge clk)
 	begin
 		case(opcode)
-			0: {Carryout,AluOut} = tempA & tempB;
-			1: {Carryout,AluOut} = tempA | tempB;
+			0: 
+			begin
+				{Carryout,AluOut} = tempA & tempB;
+				Overflow = 0;
+			end 
+			1: 
+			begin
+				{Carryout,AluOut} = tempA | tempB;
+				Overflow = 0;
+			end
 			2: 
 			begin
 				{Carryout,AluOut} = tempA + tempB;
@@ -60,9 +69,21 @@ module pipealu(instr,AluOut, Zero,clk,Carryout,Overflow);
 				Overflow=((tempA[31:31]&!tempB[31:31])&!AluOut[31:31])
 						|((!tempA[31:31]&tempB[31:31])&AluOut[31:31]);
 			end
-			7: {Carryout,AluOut} = tempA < tempB ? 1 : 0;
-			12: {Carryout,AluOut} = ~(tempA | tempB);
-			default: {Carryout,AluOut} = 0;
+			7:
+			begin
+				{Carryout,AluOut} = tempA < tempB ? 1 : 0;
+				Overflow = 0;
+			end
+			12:
+			begin
+				{Carryout,AluOut} = ~(tempA | tempB);
+				Overflow = 0;
+			end
+			default:
+			begin
+				{Carryout,AluOut} = 0;
+				Overflow = 0;
+			end
 		endcase
 		regfile[dest]=AluOut;
 		Zero = (AluOut==0);
--- HW2/pipealu_test.v
+++ HW2/pipealu_test.v
@@ -19,32 +19,55 @@ module pipealu_test;
 	);
 	
 	initial begin
-		instr = 16'b0010000000011111;//add
+		instr = 16'b0010000000011111;//add r15 = r0+r1  (test add overflow)
 		clk = 0;
 		#4;
 		clk = 1;
 		#4;
-		instr = 16'b0110000000011111;//sub
+		instr = 16'b0110000000011111;//sub r15 = r0-r1
 		clk = 0;
 		#4;
 		clk = 1;
 		#4;
-		instr = 16'b0000000000011111;//and
+		instr = 16'b0000000000011111;//and r15 = r0&r1
 		clk = 0;
 		#4;
 		clk = 1;
 		#4;
-		instr = 16'b0001000000011111;//or
+		instr = 16'b0001000000011111;//or r15 = r0|r1
 		clk = 0;
 		#4;
 		clk = 1;
 		#4;
-		instr = 16'b0111000000011111;//slt
+		instr = 16'b0111000000011111;//slt r15 = r0 slt r1
 		clk = 0;
 		#4;
 		clk = 1;
 		#4;
-		instr = 16'b1100000000011111;//nor
+		instr = 16'b1100000000011111;//nor r15 = r0 nor r1
+		clk = 0;
+		#4;
+		clk = 1;
+		#4;
+		instr = 16'b0010010101101111;//add r15 = r5+r6 (test add overflow)
+		clk = 0;
+		#4;
+		clk = 1;
+		#4;
+		clk = 0;
+		#4;
+		clk = 1;
+		#4;
+		instr = 16'b0110011110001111;//sub r15 = r7-r8 (test sub overflow)
+		clk = 0;
+		#4;
+		clk = 1;
+		#4;
+		instr = 16'b0010110011011111;//add r15 = r0+r1
+		clk = 0;
+		#4;
+		clk = 1;
+		#4;
 		clk = 0;
 		#4;
 		clk = 1;
